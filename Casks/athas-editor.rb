2e4a0f1d1ecf2c7eb1fe75d91490cd44c59b542d
Ken Banks <kenbanks.peng@gmail.com>
2025-10-29 09:24:55 -0400

    commit

--- Casks/athas-editor.rb
+++ Casks/athas-editor.rb
@@ -1,17 +1,12 @@
 cask "athas-editor" do
+  arch arm: "aarch64", intel: "x64"
+
   version "0.1.2"
+  sha256 arm:   "df49108f61f7bc5ca0d2ec0fa3cacb8ac5915983a00c226636f17d2891b995bb",
+         intel: "e82d38abaa4925b8171951428008e6f770e157f64bb9a21f3faa1d8a358dfc58"
 
-  on_arm do
-    sha256 "df49108f61f7bc5ca0d2ec0fa3cacb8ac5915983a00c226636f17d2891b995bb"
-
-    url "https://github.com/athasdev/athas/releases/download/v#{version}/Athas_#{version}_aarch64.dmg"
-  end
-  on_intel do
-    sha256 "e82d38abaa4925b8171951428008e6f770e157f64bb9a21f3faa1d8a358dfc58"
-
-    url "https://github.com/athasdev/athas/releases/download/v#{version}/Athas_#{version}_x64.dmg"
-  end
-
+  url "https://github.com/athasdev/athas/releases/download/v#{version}/Athas_#{version}_#{arch}.dmg",
+      verified: "github.com/athasdev/athas/"
   name "Athas"
   desc "AI-powered code editor"
   homepage "https://github.com/athasdev/athas"
